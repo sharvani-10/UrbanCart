3ff82b220db9758622fb85a734b870e14d63a7f1
sharvani-10 <vanaparthi.sharvani@gmail.com>
2024-12-18 15:16:09 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/urbancart/',
+  base: '/UrbanCart/',
   build: {
     chunkSizeWarningLimit: 1000,
     // Existing 'urbancart' settings (keep as it is)
