80dd376512f4945b5e48e1a39abe3072867d1636
sharvani-10 <vanaparthi.sharvani@gmail.com>
2024-12-18 14:59:31 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/urbancart/',
+  build: {
+    // Existing 'urbancart' settings (keep as it is)
+    rollupOptions: {
+      output: {
+        // Add manualChunks option here
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', 'react-redux'] // Extract these libraries into a separate chunk
+        }
+      }
+    }
+  }
 })
